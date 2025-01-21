a23b44330896f800fb83ab733e7d1ac211ffddeb
ninanikolaevna <ninanikolaevna.t@gmail.com>
2025-01-21 11:37:58 +0300

    Initial

---
+++ src/main/java/com/example/skypro_spring_listAndSet/SkyproSpringListAndSetApplication.java
@@ -0,0 +1,13 @@
+package com.example.skypro_spring_listAndSet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringListAndSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringListAndSetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skypro_spring_listAndSet/SkyproSpringListAndSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skypro_spring_listAndSet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringListAndSetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
